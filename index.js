e35720785fe16b203daacb36fdfb446472586cea
Alex <alexhewitt.j@gmail.com>
2024-04-15 07:06:50 -0500

    modify how we connect to mongodb
    exercises 3.13-3.14 next

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Note = require('./models/note')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://alexhewitt:${password}@cluster0.pxl1wm5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: 'bool'
-})
-
-const Note = mongoose.model('Note', noteSchema)
+// if (process.argv.length < 3) {
+//     console.log('give password as argument')
+//     process.exit(1)
+// }
 
 let notes = [
     {
@@ -79,32 +66,24 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
+        important: body.important || false
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then((savedNote) => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find((note) => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then((note) => {
         response.json(note)
-    } else {
-        console.log('x')
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -116,7 +95,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
