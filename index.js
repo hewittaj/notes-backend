9da8fcbad97452663afd1afe4685b012c4601589
Alex Hewitt <alexhewitt.j@gmail.com>
2024-04-20 11:43:17 -0500

    add eslint files

--- index.js
+++ index.js
@@ -9,93 +9,94 @@ app.use(express.json())
 app.use(cors())
 
 const requestLogger = (request, response, next) => {
-	console.log('Method:', request.method)
-	console.log('Path:  ', request.path)
-	console.log('Body:  ', request.body)
-	console.log('---')
-	next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.get('/', (request, response) => {
-	response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-	Note.find({}).then((notes) => {
-		response.json(notes)
-	})
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
 })
 
 app.post('/api/notes', (request, response, next) => {
-	const body = request.body
+  const body = request.body
 
-	if (body.content === undefined) {
-		return response.status(400).json({ error: 'content missing' })
-	}
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-	const note = new Note({
-		content: body.content,
-		important: body.important || false
-	})
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
 
-	note.save()
-		.then((savedNote) => {
-			response.json(savedNote)
-		})
-		.catch((error) => next(error))
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-	Note.findById(request.params.id)
-		.then((note) => {
-			if (note) {
-				response.json(note)
-			} else {
-				response.status(404).end()
-			}
-		})
-		.catch((error) => next(error))
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-	Note.findByIdAndDelete(request.params.id)
-		.then((result) => {
-			response.status(204).end()
-		})
-		.catch((error) => next(error))
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-	const body = request.body
-	const note = {
-		content: body.content,
-		important: body.important
-	}
-	Note.findByIdAndUpdate(request.params.id, note, { new: true })
-		.then((updatedNote) => {
-			response.json(updatedNote)
-		})
-		.catch((error) => next(error))
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
 })
 function errorHandler(error, request, response, next) {
-	console.log(error.message)
+  console.log(error.message)
 
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	} else if (error.name === 'ValidationError') {
-		return response.status(400).json({ error: error.message })
-	}
-	next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
