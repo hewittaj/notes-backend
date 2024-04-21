e253c25790d7857dafa22cc7485623482241012c
Alex Hewitt <alexhewitt.j@gmail.com>
2024-04-21 13:55:41 -0500

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,10 @@ module.exports = {
     '@stylistic/js/indent': ['error', 2],
     '@stylistic/js/linebreak-style': ['error', 'unix'],
     '@stylistic/js/quotes': ['error', 'single'],
-    '@stylistic/js/semi': ['error', 'never']
+    '@stylistic/js/semi': ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }]
   }
 }
